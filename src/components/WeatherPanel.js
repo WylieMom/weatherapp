30b21e035985994487490fa8c883b17903b22682
WylieMom <50558542+WylieMom@users.noreply.github.com>
2023-05-01 17:14:31 -0500

    Update WeatherPanel.js

--- src/components/WeatherPanel.js
+++ src/components/WeatherPanel.js
@@ -1,78 +1,65 @@
-import React, {useState} from 'react';
-import Form from './Form';
-import Card from './Card';
+import React, { useState, useEffect } from "react";
+import Form from "./Form";
+import Card from "./Card";
 
 const WeatherPanel = () => {
-    let urlWeather = "https://api.openweathermap.org/data/2.5/weather?appid=0d7c6a5e9ff1afa1d1201838450f6b5d&units=imperial";
+  const [weather, setWeather] = useState([]);
+  const [forecast, setForecast] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(false);
+  const [location, setLocation] = useState("");
 
-    let cityURL = "&q=";
-
-    let urlForecast = "https://api.openweathermap.org/data/2.5/forecast?appid=0d7c6a5e9ff1afa1d1201838450f6b5d&units=imperial";
-
-    const [weather, setWeather] = useState([]);
-    const [forecast, setForecast] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [show, setShow] = useState(false);
-    const [location, setLocation] = useState("");
-
-    const getLocation = async(loc) => {
-        setLoading(true);
-        setLocation(loc);
-
-        //weather
-
-        urlWeather = urlWeather + cityURL + loc;
-
-        await fetch(urlWeather).then((response) =>{
-            if (!response.ok) throw {response}
-            return response.json();
-        }).then((weatherData) =>{
-            console.log(weatherData);
-            setWeather(weatherData);
-        }).catch(error =>{
-            console.log(error);
-            setLoading(false);
-            setShow(false);
-        });
-
-        //Forcast
-
-        urlForecast = urlForecast + cityURL + loc;
-
-        await fetch(urlForecast).then((response) =>{
-            if (!response.ok) throw {response}
-            return response.json();
-        }).then((forecastData) =>{
-            console.log(forecastData);
-            setForecast(forecastData);
-
-            setLoading(false);
-            setShow(true);
-
-        }).catch(error =>{
-            console.log(error);
-            setLoading(false);
-            setShow(false);
-        });
-
+  const getWeatherData = async (location) => {
+    const urlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=0d7c6a5e9ff1afa1d1201838450f6b5d&units=imperial&q=${location}`;
+    const response = await fetch(urlWeather);
+    if (!response.ok) {
+      throw new Error(response.statusText);
     }
+    const data = await response.json();
+    setWeather(data);
+  };
 
-    return(
-        <React.Fragment>
+  const getForecastData = async (location) => {
+    const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?appid=0d7c6a5e9ff1afa1d1201838450f6b5d&units=imperial&q=${location}`;
+    const response = await fetch(urlForecast);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    setForecast(data);
+  };
 
-            <Form 
-            newLocation = {getLocation}
-            />
+  useEffect(() => {
+    if (location) {
+      setLoading(true);
+      Promise.all([getWeatherData(location), getForecastData(location)])
+        .then(() => {
+          setLoading(false);
+          setShow(true);
+        })
+        .catch((error) => {
+          console.log(error);
+          setLoading(false);
+          setShow(false);
+        });
+    }
+  }, [location]);
 
-            <Card 
-            showData = {show}
-            loadingData = {loading}
-            weather = {weather}
-            forecast = {forecast}
-            />
+  const handleLocationChange = (loc) => {
+    setLocation(loc);
+  };
 
-        </React.Fragment>
-    );
-}
+  return (
+    <>
+      <Form newLocation={handleLocationChange} />
+      <Card
+        showData={show}
+        loadingData={loading}
+        weather={weather}
+        forecast={forecast}
+      />
+    </>
+  );
+};
 
 export default WeatherPanel;
